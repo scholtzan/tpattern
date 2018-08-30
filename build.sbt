987c014db7ff9310d1a625d6887fefdd5cb8f316
Anna Scholtz <anna@scholtzan.net>
2018-08-30 11:25:18 -0700

    Project setup

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "tpattern"
+
+version := "1.0"
+
+scalaVersion := "2.12.6"
+    
---
+++ src/main/scala/net/scholtzan/tpattern/TPattern.scala
@@ -0,0 +1,8 @@
+package net.scholtzan.tpattern
+
+/**
+  * Created by anna on 30.08.18.
+  */
+class TPattern {
+
+}
